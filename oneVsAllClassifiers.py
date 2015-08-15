bceedb57bce86837cdb7d7824b9549ae63504d5a
shpach <shandilya.pachgade@gmail.com>
2015-08-15 03:47:31 -0400

    final/near-final version of SVM algorithm. Added some basic analytics. Kaggle output file is available after running.

--- oneVsAllClassifiers.py
+++ oneVsAllClassifiers.py
@@ -1,4 +1,5 @@
 import sys
+import pprint
 import pandas as pd
 import numpy as np
 import time
@@ -23,7 +24,10 @@ def main():
     #Timing SVM creation
     startTime = time.time()
 
+
     #create a SVM with linear kernel
+
+    #variety of C tried, none made significant difference, even after overfitting training data, default constructer used
     linearSVM = svm.LinearSVC()
     linearSVM.fit(X_train,y)
     endTime = time.time() - startTime
@@ -34,21 +38,46 @@ def main():
 
 
     #predict training set digits
-    predictions = linearSVM.predict(X_train)
+    predictions = linearSVM.predict(X_train).astype(int)
 
-    print(predictions)
-    print(y)
+    print(("Predictions: {}").format(predictions))
+    print(("Actual: {}").format(y.astype(int)))
 
-    #accuracy test
+
+
+
+    #algorithm analysis, debugging, and other info
     correct = 0.0
+    numberOfDigits = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0}
+    numberOfDigitsWrong = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0}
+
+    #iterate through all examples
     for index in range(y.shape[0]):
         if y[index] == predictions[index]:
             correct+=1
-    print(("Accuracy: {}").format(correct/y.shape[0]))
+        else:
+            numberOfDigitsWrong[int(y[index])]+=1
+        numberOfDigits[int(y[index])]+=1
+        
+    #results of analysis
+    print(("Accuracy of algorithm on training data: {}").format(correct/y.shape[0]))
+    print(("Number of each digit: {}").format(numberOfDigits))
+    print(("Number of each digit predicted incorrectly: {}").format(numberOfDigitsWrong))
+
+
+
+
 
     #test predictions
-    return linearSVM.predict(X_test)
+    predictTest = (linearSVM.predict(X_test)).astype(int)
+    imgid = np.zeros((predictTest.shape[0],1))
+    for n in range(0, predictTest.shape[0]):
+        imgid[n] = n + 1
+    output = np.c_[imgid, predictTest]
+
+    np.savetxt('output.csv', output, fmt="%i", delimiter= ",", header = "ImageId,Label", comments = "")
+    return predictTest
 
 
 if __name__ == '__main__':
-	main()
+    main()
