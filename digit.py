327d76e6c8148668079a16c124b4dd48c23421ad
Matthew Zang <matthewzang1@Matthews-MacBook-Pro-2.local>
2015-08-15 09:49:07 -0700

    finished one for all log regression

--- digit.py
+++ digit.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import numpy as np
-import sklearn.multiclass.OneVsRestClassifier as oneVsAll
+import math
 from scipy import optimize
+from scipy.sparse import *
 
 def main():
     # The competition datafiles are in the directory ../input
@@ -19,29 +20,39 @@ def main():
 
     #Train multiclass logistic regression
     print("Training multiclass logistic regression...")
-    lamda = 0.1
+    lamda = 0.085
     theta = oneVsAll(X_trainer, y, num_classes, lamda);
+    predict = predicter(theta, X_tester)
+    imgid = np.zeros((predict.shape[0],1))
+    for n in range(0, predict.shape[0]):
+        imgid[n] = n + 1
+    predict = np.c_[imgid, predict]
+    np.savetxt('predict.csv', predict, fmt="%i", delimiter= ",", header = "ImageId,Label", comments = "")
 
 def sigmoid(X):
-    sig = 1.0 + np.exp(-1.0 * X)
+    sig = 1.0 + math.e ** (-1.0 * X)
     sig = 1.0 / sig
+    sig[sig == 0] = 1.0 / (10 ** 20)
     return sig
 
 
-# def oneVsAll(X_trainer, y, num_classes, lamda):
-#     (m, n) = X_trainer.shape
-#     #initialize theta to be empty
-#     full_theta = np.empty(shape=[n + 1, num_classes])
-#     #add column of 1's to X
-#     X_trainer = np.c_[np.ones((m,1)), X_trainer]
-#     for num in range(1, num_classes + 1):
-#         print("Training class number " + str(num))
-#         initial_theta = np.zeros((n + 1, 1))
-#         y_num = find_vector(y, num)
-#         args = (X_trainer, y_num, lamda, m, n)
-#         print("Starting optimization...")
-#         theta = optimize.fmin_cg(cost, initial_theta, fprime = grad, args = args)
-#     print(full_theta)
+def oneVsAll(X_trainer, y, num_classes, lamda):
+    (m, n) = X_trainer.shape
+    #initialize theta to be empty
+    full_theta = np.empty(shape=[num_classes, n + 1])
+    #add column of 1's to X
+    X_trainer = np.c_[np.ones((m,1)), X_trainer]
+    for num in range(0, num_classes):
+        print("Training class number " + str(num))
+        initial_theta = np.zeros((n + 1, 1))
+        y_num = find_vector(y, num)
+        args = (X_trainer, y_num, lamda, m, n)
+        #cost(initial_theta, X_trainer, y_num, lamda, m, n)
+        #grad(initial_theta, X_trainer, y_num, lamda, m, n)
+        theta = optimize.fmin_cg(cost, initial_theta, fprime = grad, args = args, maxiter = 100)
+        full_theta[num] = theta
+    print(full_theta)
+    return full_theta
 
 def find_vector(y, num):
     m = y.shape[0]
@@ -53,14 +64,36 @@ def find_vector(y, num):
 
 def cost(theta, *args):
     X_trainer, y, lamda, m, n = args
-    J = 1/m * (-1 * np.multiply(y, np.log(sigmoid(np.dot(X_trainer , theta)))) - np.multiply((1 - y), np.log(1-sigmoid(np.dot(X_trainer , theta))))).sum() + lamda/(2 * m) * ((theta ** 2).sum() - theta[0] ** 2)
+    theta = theta.reshape((n+1, 1))
+    X_sparse = csc_matrix(X_trainer)
+    h = sigmoid(X_sparse * theta)
+    #print("Calculating cost...")
+    logh = np.log(h).reshape((m, 1))
+    onelogh = np.log(1-h).reshape((m, 1))
+    J = 1/m * (-1 * np.multiply(y, logh) - np.multiply((1 - y), onelogh)).sum() + lamda/(2 * m) * ((theta ** 2).sum() - theta[0] ** 2)
+    #print(J)
     return J
 
 def grad(theta, *args):
     X_trainer, y, lamda, m, n = args
-    a = np.ones((n, 1))
+    theta = theta.reshape((n+1, 1))
+    a = np.ones((n+1, 1))
     a[0] = 0
-    grad = 1/m * np.dot(X_trainer.T, sigmoid(np.dot(X_trainer, theta) - y)) + lamda/m * np.multiply(theta, a)
+    X_sparse = csc_matrix(X_trainer)
+    h = sigmoid(X_sparse * theta)
+    error = h.reshape((m,1)) - y
+    X_sparse = csc_matrix(X_trainer.T)
+    #print("Calculating gradient...")
+    grad = 1/m * X_sparse * error +lamda/m * np.multiply(theta, a)
+    grad = grad.flatten()
     return grad
+
+def predicter(theta, X_tester):
+    m = X_tester.shape[0]
+    X_tester = np.c_[np.ones((m,1)), X_tester]
+    X_sparse = csc_matrix(X_tester)
+    y_test = X_sparse * theta.T
+    predict = y_test.argmax(1)
+    return predict
 
 main()
